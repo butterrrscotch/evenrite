91f5628a43e3384272b33ba150e92c565b9cf4d5
butterrrscotch <nadyadhyanti@gmail.com>
2024-03-24 02:50:33 +0700

    fixing errors

--- app/teams/components/Teams.tsx
+++ app/teams/components/Teams.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 
-const TeamCard = ({ props }) => {
+interface TeamCardProps {
+  picture?: {
+    medium: string;
+  };
+  name: {
+    first: string;
+    last: string;
+  };
+  location: {
+    city: string;
+  };
+  email: string;
+}
+
+const TeamCard: React.FC<TeamCardProps> = ({ picture, name, location, email }) => {
   return (
     <div className="max-w-md mb-10 items-center">
       <div className="bg-gray-90 rounded-lg shadow-lg overflow-hidden">
         <div className="px-6 py-4">
           <div className="flex items-center justify-center">
-            <img className="w-24 h-24 rounded-full" src={props.picture.medium} alt={`${props.name.first} ${props.name.last}`} />
+            {picture && picture.medium && (
+              <img className="w-24 h-24 rounded-full" src={picture.medium} alt={`${name.first} ${name.last}`} />
+            )}
           </div>
           <div className="text-center mt-4">
-            <h2 className="text-lg font-semibold text-white">{props.name.first} {props.name.last}</h2>
-            <p className="text-sm text-amber-400">{props.location.city}</p>
+            <h2 className="text-lg font-semibold text-white">{name.first} {name.last}</h2>
+            <p className="text-sm text-amber-400">{location.city}</p>
           </div>
           <div className="flex mt-2 text-center items-center">
-            <p className="text-sm text-white text-center">{props.email}</p>
+            <p className="text-sm text-white text-center">{email}</p>
           </div>
         </div>
       </div>
--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -11,8 +11,8 @@ const Teams = () => {
       <h1 className="bold-40 text-center mb-5">For Life Teams. Teams For Life.</h1>
       <p className="regular-16 text-gray-30 text-center mb-10 mr-20 ml-20">At For Life Group, our team is the driving force behind our success. Each member plays a crucial role in creating memorable experiences for our guests. Meet the faces behind For Life Group.</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {dataUsers?.results.map((props: any) => (
-          <TeamCard key={props.id.value} props={props} />
+        {dataUsers?.results?.map((user: any) => (
+          <TeamCard key={user.id.value} {...user} />
         ))}
       </div>
     </div>
