096e0999f7b6a15a2c0717f596816a89e8202800
butterrrscotch <nadyadhyanti@gmail.com>
2024-03-24 03:30:36 +0700

    optimizing images

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -3,32 +3,32 @@ import styles from './Brands.module.css';
 
 const Brands = () => {
   return (
-    <div className="flex flex-col items-center mt-10">
-      <h2 className="text-xl font-bold text-black mb-4 mt-4">Our Brands</h2>
+    <div className="flex flex-col items-center mt-10 mb-10">
+      <h2 className="bold-32 font-bold text-black mb-6 mt-4">Our Brands</h2>
       <div className={styles.marquee}>
-        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-4" />
+        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-6" />
         <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24" />
-        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-4" />
+        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-6" />
         <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24" />
-        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-4" />
+        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-6" />
         <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24" />
-        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-4" />
+        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-6" />
         <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24" />
-        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-4" />
-        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-4" />
+        <img src="/Marquee_RFL.png" alt="Roti Bakar For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_CFL.png" alt="Coffee For Life" className="w-auto h-24 mr-6" />
+        <img src="/Marquee_EFL.png" alt="Es Buah For Life" className="w-auto h-24 mr-6" />
         <img src="/Marquee_WFL.png" alt="Warmindo For Life" className="w-auto h-24" />
       </div>
     </div>
